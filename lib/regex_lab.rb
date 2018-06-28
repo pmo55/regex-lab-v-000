8c65b72f50a68c41005fe14344dba9aba59c03fc
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-06-28 01:39:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,22 @@
 def starts_with_a_vowel?(word)
-
+ if word.match(/^[aeiouAEIOU]/)
+   return true
+ else return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
-
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/\A[A-Z]*[?.!]/)
+  return true
+else return false
+end
 end
 
 def valid_phone_number?(phone)
